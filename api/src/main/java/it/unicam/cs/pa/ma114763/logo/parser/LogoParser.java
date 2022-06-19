81b4af4b2de06980fb574f48c0220797d4c9328b
Lorenzo Lapucci <halexercraft@gmail.com>
2022-06-19 17:42:21 +0200

    Split lines and trim strings before getting the token

--- api/src/main/java/it/unicam/cs/pa/ma114763/logo/parser/LogoParser.java
+++ api/src/main/java/it/unicam/cs/pa/ma114763/logo/parser/LogoParser.java
@@ -3,6 +3,7 @@ package it.unicam.cs.pa.ma114763.logo.parser;
 import it.unicam.cs.pa.ma114763.logo.parser.exception.InvalidCharactersException;
 import it.unicam.cs.pa.ma114763.logo.parser.exception.ParserException;
 import it.unicam.cs.pa.ma114763.logo.parser.tokentype.*;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -24,8 +25,23 @@ public class LogoParser implements Parser {
 
     @Override
     public List<Statement> parse(String input) throws ParserException {
+        List<Statement> statements = new ArrayList<>();
+        // every line is parsed as a statement
+        String[] lines = input.split("\n");
+        for (String line : lines) {
+            String trimmed = line.trim();
+            // skip if empty line or a comment
+            if (trimmed.isEmpty() || trimmed.startsWith("#")) {
+                continue;
+            }
+            statements.add(parseLine(trimmed));
+        }
+        return statements;
+    }
+
+    private Statement parseLine(String line) throws ParserException {
         // split the input string on whitespace
-        String[] strings = input.split("\\s+");
+        String[] strings = line.split("\\s+");
         List<Token> tokens = getTokens(strings);
         return null;
     }
@@ -34,13 +50,22 @@ public class LogoParser implements Parser {
         List<Token> tokens = new ArrayList<>();
 
         for (String string : strings) {
-            Token token = tokenizer.matchToken(string);
-            if (token == null) {
-                throw new InvalidCharactersException(string);
+            String trimmed = string.trim();
+            // skip empty strings
+            if (trimmed.isEmpty()) {
+                continue;
             }
-            tokens.add(token);
+            tokens.add(getToken(trimmed));
         }
 
         return tokens;
+    }
+
+    private @NotNull Token getToken(String string) throws InvalidCharactersException {
+        Token token = tokenizer.matchToken(string);
+        if (token == null) {
+            throw new InvalidCharactersException(string);
+        }
+        return token;
     }
 }
