693ff1aab255391bfbbd94e710acb079549952b6
Lorenzo Lapucci <halexercraft@gmail.com>
2022-06-22 18:35:27 +0200

    Update UI tests with the latest tests

--- ui/src/main/java/it/unicam/cs/pa/ma114763/logo/ui/LogoUI.java
+++ ui/src/main/java/it/unicam/cs/pa/ma114763/logo/ui/LogoUI.java
@@ -1,11 +1,13 @@
 package it.unicam.cs.pa.ma114763.logo.ui;
 
 import it.unicam.cs.pa.ma114763.logo.LogoProcessor;
+import it.unicam.cs.pa.ma114763.logo.Point;
 import it.unicam.cs.pa.ma114763.logo.RGBColor;
-import it.unicam.cs.pa.ma114763.logo.Statement;
+import it.unicam.cs.pa.ma114763.logo.shape.Line;
+import it.unicam.cs.pa.ma114763.logo.shape.Polygon;
+import it.unicam.cs.pa.ma114763.logo.statement.Statement;
 import it.unicam.cs.pa.ma114763.logo.parser.LogoParser;
 import it.unicam.cs.pa.ma114763.logo.parser.exception.ParserException;
-import it.unicam.cs.pa.ma114763.logo.shape.Shape;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
@@ -13,6 +15,7 @@ import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 import java.util.List;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 /**
  * @author Lorenzo Lapucci
@@ -23,7 +26,22 @@ public class LogoUI extends Application {
         int width = 800;
         int height = 600;
 
-        String program = "setpencolor 121 153 237\nrepeat 18 [repeat 5 [rt 40 fd 200 rt 120] rt 20]";
+        String program = """
+                setpencolor 121 153 237
+                setfillcolor 121 153 237 120
+                repeat 18 [repeat 5 [rt 40 fd 200 rt 120] rt 20]
+                home
+                right 90
+                penup
+                backward 100
+                pendown
+                backward 100
+                right 60
+                forward 100
+                left 120
+                setfillcolor 237 153 121
+                forward 100
+                """;
 
         Canvas canvas = new Canvas(width, height);
         Scene scene = new Scene(new StackPane(canvas), width, height);
@@ -36,14 +54,27 @@ public class LogoUI extends Application {
         FXDrawing drawing = new FXDrawing(canvas, width, height);
         processor.execute(statements, drawing);
 
-        canvas.setOnMouseClicked(event -> {
-            drawing.setBackgroundColor(new RGBColor(0, 0, 0));
-            drawing.setStrokeColor(new RGBColor(255, 255, 255));
+        AtomicBoolean flag = new AtomicBoolean(false);
 
-            List<Shape> shapes = drawing.getShapes();
-            for (Shape shape : shapes) {
-                shape.draw(drawing);
+        canvas.setOnMouseClicked(event -> {
+            if (flag.compareAndSet(false, true)) {
+                drawing.setBackgroundColor(new RGBColor(0, 0, 0));
+            } else {
+                drawing.setBackgroundColor(new RGBColor(255, 0, 255));
+                flag.set(false);
             }
+
+            List<Line> strokes = List.of(
+                    new Line(new Point(-200, 20), new Point(110, 210), 1, new RGBColor(255, 255, 255)),
+                    new Line(new Point(110, 210), new Point(120, 220), 1, new RGBColor(255, 255, 255)),
+                    new Line(new Point(120, 220), new Point(130, 230), 1, new RGBColor(255, 255, 255)),
+                    new Line(new Point(130, 230), new Point(140, 240), 1, new RGBColor(255, 0, 0)),
+                    new Line(new Point(140, 240), new Point(145, 245), 1, new RGBColor(0, 0, 255)),
+                    new Line(new Point(145, 245), new Point(145, 200), 1, new RGBColor(255, 255, 255)),
+                    new Line(new Point(145, 200), new Point(-200, 20), 1, new RGBColor(255, 255, 255))
+            );
+            Polygon polygon = new Polygon(new RGBColor(255, 255, 255, 127), strokes);
+            polygon.draw(drawing);
         });
 
 //        LinkedList<Statement> queue = new LinkedList<>(statements);
