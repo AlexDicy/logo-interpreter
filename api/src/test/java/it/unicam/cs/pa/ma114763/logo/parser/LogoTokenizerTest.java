39b701246d7895751b3536aea0f6aba855e0fd18
Lorenzo Lapucci <halexercraft@gmail.com>
2022-06-24 21:00:51 +0200

    Add tests for LogoTokenizer

---
+++ api/src/test/java/it/unicam/cs/pa/ma114763/logo/parser/LogoTokenizerTest.java
@@ -0,0 +1,61 @@
+package it.unicam.cs.pa.ma114763.logo.parser;
+
+import it.unicam.cs.pa.ma114763.logo.parser.tokentype.WordTokenType;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.regex.Pattern;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertInstanceOf;
+
+/**
+ * @author Lorenzo Lapucci
+ */
+class LogoTokenizerTest {
+    private static LogoTokenizer tokenizer;
+    private static List<TokenType> types;
+
+    @BeforeAll
+    static void setUp() {
+        types = List.of(
+                new TokenType("forward", Pattern.compile("^forward$")) {
+                },
+                new TokenType("left", Pattern.compile("^left$")) {
+                },
+                new TokenType("decimal", Pattern.compile("^[0-9]+(?:\\.[0-9]+)?$")) {
+                },
+                new WordTokenType()
+        );
+        tokenizer = new LogoTokenizer(types);
+    }
+
+    @Test
+    void testEmptyMatch() {
+        List<Token> t1 = tokenizer.matchTokens("forward x");
+        assertEquals(0, t1.size());
+        List<Token> t2 = tokenizer.matchTokens("-1.2");
+        assertEquals(0, t2.size());
+    }
+
+    @Test
+    void testMatchTokens() {
+        List<Token> t1 = tokenizer.matchTokens("forward");
+        assertToken(t1, 0, "forward");
+        List<Token> t2 = tokenizer.matchTokens("left");
+        assertToken(t2, 1, "left");
+        List<Token> t3 = tokenizer.matchTokens("1.0");
+        assertToken(t3, 2, "1.0");
+        List<Token> t4 = tokenizer.matchTokens("17");
+        assertToken(t4, 2, "17");
+        List<Token> t5 = tokenizer.matchTokens("randomWord");
+        assertToken(t5, 3, "randomWord");
+    }
+
+    private static void assertToken(List<Token> tokens, int tokenTypeIndex, String text) {
+        assertEquals(1, tokens.size());
+        assertInstanceOf(types.get(tokenTypeIndex).getClass(), tokens.get(0).type());
+        assertEquals(text, tokens.get(0).text());
+    }
+}
