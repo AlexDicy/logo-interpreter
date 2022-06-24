693e2ef8ceb7a484d1518c9b6e0e0bae63ba6f6d
Lorenzo Lapucci <halexercraft@gmail.com>
2022-06-24 12:06:31 +0200

    Start working on Reader/Writer interfaces

---
+++ api/src/main/java/it/unicam/cs/pa/ma114763/logo/io/Reader.java
@@ -0,0 +1,9 @@
+package it.unicam.cs.pa.ma114763.logo.io;
+
+/**
+ * Reads a resource and returns a string representing the program.
+ *
+ * @author Lorenzo Lapucci
+ */
+public interface Reader {
+}
---
+++ api/src/main/java/it/unicam/cs/pa/ma114763/logo/io/Writer.java
@@ -0,0 +1,9 @@
+package it.unicam.cs.pa.ma114763.logo.io;
+
+/**
+ * Takes a string representing a program and saves it in a resource.
+ *
+ * @author Lorenzo Lapucci
+ */
+public interface Writer {
+}
