b3e5b3cb835e5f5c0864f96ead30f4843f46c1db
Lorenzo Lapucci <halexercraft@gmail.com>
2022-06-24 12:10:51 +0200

    Add read() and write() methods

--- api/src/main/java/it/unicam/cs/pa/ma114763/logo/io/Reader.java
+++ api/src/main/java/it/unicam/cs/pa/ma114763/logo/io/Reader.java
@@ -1,9 +1,19 @@
 package it.unicam.cs.pa.ma114763.logo.io;
 
+import java.io.IOException;
+
 /**
  * Reads a resource and returns a string representing the program.
  *
  * @author Lorenzo Lapucci
  */
 public interface Reader {
+
+    /**
+     * Reads the program string from the resource
+     *
+     * @return the string representing the program
+     * @throws IOException if an error occurred while reading from the resource
+     */
+    String read() throws IOException;
 }
--- api/src/main/java/it/unicam/cs/pa/ma114763/logo/io/Writer.java
+++ api/src/main/java/it/unicam/cs/pa/ma114763/logo/io/Writer.java
@@ -1,9 +1,19 @@
 package it.unicam.cs.pa.ma114763.logo.io;
 
+import java.io.IOException;
+
 /**
  * Takes a string representing a program and saves it in a resource.
  *
  * @author Lorenzo Lapucci
  */
 public interface Writer {
+
+    /**
+     * Writes the given string to the resource.
+     *
+     * @param program the string to write
+     * @throws IOException if an error occurred while writing to the resource
+     */
+    void write(String program) throws IOException;
 }
