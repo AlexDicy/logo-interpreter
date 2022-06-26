51524a41c50878d1e59cec4324b505ef17cc62a8
Lorenzo Lapucci <halexercraft@gmail.com>
2022-06-26 18:35:50 +0200

    Add a wrapper for JavaFX Main class

---
+++ ui/src/main/java/it/unicam/cs/pa/ma114763/logo/ui/JARMain.java
@@ -0,0 +1,10 @@
+package it.unicam.cs.pa.ma114763.logo.ui;
+
+/**
+ * @author Lorenzo Lapucci
+ */
+public class JARMain {
+    public static void main(String[] args) {
+        LogoUI.main(args);
+    }
+}
